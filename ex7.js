a65b89134a0a2997f045976dcf7be8266ce0c4e7
Nicoly07 <nicolyravagnani07@gmail.com>
2020-08-25 17:44:15 -0300

    ex7 realizado

---
+++ ex7.js
@@ -0,0 +1,17 @@
+function ex7() {
+    principal7() 
+}
+function principal7() {
+    var soma = 0
+    var contador = 0
+    var numero = Number(prompt(`Informe um número`))
+    do {
+        contador++
+        soma = soma + numero
+        var numero = Number(prompt(`Informe outro número. Digite um número negativo para encerrar!`))
+    }
+    while (numero >= 0)
+
+    console.log(`A média dos números é: ${soma/contador}`)
+}
+ex7()
